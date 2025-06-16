3f113922d678ba7caa8cd3c223539fd17c07074f
Fletacode <bassykd@gmail.com>
2025-06-16 15:19:42 +0900

    chore: .gitignore에 __pycache__ 추가 및 main.py에서 호스트와 포트 환경변수 설정

--- main.py
+++ main.py
@@ -132,4 +132,6 @@ async def get_scraper_categories(authorization: str = Header(None)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    host = os.getenv("HOST", "0.0.0.0")
+    port = os.getenv("PORT", 8000)
+    uvicorn.run("main:app", host=host, port=port, reload=True)
